30ea64941305e2fce7144d042a8fcbe54fb4155f
Nadav Blum <nadav.blum@gmail.com>
2011-11-19 00:09:09 +0200

    Minor refactoring

--- presentor.js
+++ presentor.js
@@ -1,12 +1,14 @@
 Presentor = {
 
+  EMPTY_SIGN: "--",
+
   queue: new Array(),
 
   run: function() {
     var cmd;
     if(Presentor.queue.length > 0) {
       cmd = Presentor.queue.shift();
-      cmd.call(cmd);
+      cmd.call(this);
       setTimeout(Presentor.run, 500);
     }
   },
@@ -15,7 +17,7 @@ Presentor = {
          this.queue.push(function() {
          var id = "_" + index;
          var elem = $("#" + id);
-         if (value === null) value = "--"; // Null is no displayed
+         if (value === null) value = Presentor.EMPTY_SIGN; // Null is no displayed
          if(elem.length > 0) {  // Element with the given id found - update it's text
            elem.text(value);
          } else {  // Create the element
@@ -49,29 +51,36 @@ Presentor = {
 
   before_compare: function (index_a, index_b) {
        this.queue.push(function() {
-         var id_a = index_a < 0  ? "fetched" : index_a;
-         var id_b = index_b < 0  ? "fetched" : index_b;
-         $("#_" + id_a + ",#_" + id_b).addClass("compared");
-         // $("#_" + id_a + ", #_" + id_b).delay(800).fadeTo(1000, 0.5).fadeTo(1000,1);
+         var element_a =  Presentor.Util.element(index_a);
+         var element_b =  Presentor.Util.element(index_b);
+         element_a.add(element_b).addClass("compared");
        });
   },
 
   compared: function(index_of_larger) {
        this.queue.push(function() {
-         var id = index_of_larger == -1 ? "_fetched" : "_" + index_of_larger;
-         //$("#" + id).fadeTo(400, 0.2).fadeTo(400, 1);
          $(".compared").removeClass("compared");
        });
   },
 
   moved: function(from, to, from_value, to_value) {
        this.queue.push(function() {
-         var from_id = (from == -1 ? "_fetched" : "_" + from);
-         var to_id = (to == -1 ? "_fetched" : "_" + to);
-         from_value = from_value == null ? "--" : from_value;
-         to_value = to_value == null ? "--" : to_value;
-         $("#" + from_id).text(from_value);
-         $("#" + to_id).text(to_value);
+         var from_elem = Presentor.Util.element(from);
+         var to_elem = Presentor.Util.element(to);
+
+         from_value = (from_value == null ? Presentor.EMPTY_SIGN : from_value);
+         to_value = (to_value == null ? Presentor.EMPTY_SIGN : to_value);
+
+         from_elem.text(from_value);
+         to_elem.text(to_value);
        });
+    },
+
+  // Utility
+  Util: {
+    element: function(index) {
+        return(index < 0 ? $("#_fetched") : $("#_" + index));
     }
+  }
+
 }
